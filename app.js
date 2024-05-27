ea5270fcf5089091c7d5f03c10b264c6735487c3
xsm3z <xsm.zhang@gmail.com>
2024-05-27 16:21:55 -0400

    2nd commit, Cached DOM

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
 
+
+
 /*-------------------------------- Variables --------------------------------*/
 
+
+
 /*------------------------ Cached Element References ------------------------*/
+
+const displayElement = document.querySelector('.display')
+const numberButtonElement = document.querySelectorAll('.button-number')
+const operatorButtonElement = document.querySelectorAll('.button-operator')
+const clearButtonElement = document.querySelector('.button-clear')
+const equalButtonElement = document.querySelector('.button-equals')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
